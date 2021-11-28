1be6bcbe606e0a6fbf9c0cac9b68737b92aa91ae
Leonardo Santos <leonardo.l.santos@gmail.com>
2021-11-28 11:42:06 -0300

    first commit

---
+++ mpt-noticias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mpt-noticias
+//
+//  Created by LEONARDO LUIZ SILVA SANTOS on 28/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
