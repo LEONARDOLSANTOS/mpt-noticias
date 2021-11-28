3ef9c88faf4cbee56f0e1fa0113cd3e0948cb179
Leonardo Santos <leonardo.l.santos@gmail.com>
2021-11-28 16:47:21 -0300

    homepage - terms of use

---
+++ mpt-noticias/Home/TermsOfUseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TermsOfUseViewController.swift
+//  mpt-noticias
+//
+//  Created by LEONARDO LUIZ SILVA SANTOS on 28/11/21.
+//
+
+import UIKit
+
+class TermsOfUseViewController: UIViewController {
+
+    @IBOutlet var lbTerms: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configLayout()
+    }
+    
+    func configLayout(){
+       
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
